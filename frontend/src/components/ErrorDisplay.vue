ef363ae9b2ec4c98920c1c10e2028fbfe85c1fb5
Maria Mair <mm225mz@student.lnu.se>
2026-01-12 23:46:23 +0100

    Add error och info display

---
+++ frontend/src/components/ErrorDisplay.vue
@@ -0,0 +1,47 @@
+<script setup>
+  import ErrorIcon from './icons/IconError.vue';
+
+  defineProps({
+    message: {
+      type: String,
+      required: true,
+    },
+  })
+</script>
+
+<template>
+  <div class="error-display">
+    <i>
+      <ErrorIcon/>
+    </i>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+.error-display {
+  background-color: var(--color-background-alert);
+  margin-top: 1rem;
+  padding: 0.8rem;
+  display: flex;
+  border: 1px solid var(--color-border);
+  border-radius: 10px;
+  min-width: 450px;
+}
+
+p {
+  font-size: 1rem;
+  margin-left: 1rem;
+  color: var(--color-text-dark-on-light);
+}
+
+i {
+  display: flex;
+  place-items: center;
+  place-content: center;
+  width: 32px;
+  height: 32px;
+  color: var(--color-text-dark-on-light);
+}
+
+</style>
---
+++ frontend/src/components/InfoDisplay.vue
@@ -0,0 +1,29 @@
+<script setup>
+defineProps({
+  message: {
+    type: String,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <div class="info-display">
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<style scoped>
+.info-display {
+  background-color: var(--color-background-info);
+  margin: 1rem 0;
+  padding: 0.8rem 1.5rem;
+  border: 1px solid var(--color-border-dark);
+  border-radius: 10px;
+}
+
+p {
+  font-size: 1rem;
+  color: var(--color-text-dark-on-light)
+}
+</style>
