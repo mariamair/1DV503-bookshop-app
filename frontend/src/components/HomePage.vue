591f259accb922e0f7fce7083bd0a9d57f8291da
Maria Mair <mm225mz@student.lnu.se>
2026-01-17 08:43:38 +0100

    Add main page

---
+++ frontend/src/components/HomePage.vue
@@ -0,0 +1,11 @@
+<template>
+  <img alt="Book shop logo" class="logo" src="../assets/bookstore.png" width="300px" height="300px" />
+</template>
+
+<style scoped>
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+  background-color: #fff;
+}
+</style>
